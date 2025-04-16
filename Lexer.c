a2c8dcd4bc7fbe45d0c85dc9e7b048f2074a9451
zappro74 <150842322+zappro74@users.noreply.github.com>
2025-04-15 21:31:37 -0500

    Create Lexer

---
+++ Lexer.c
@@ -0,0 +1,86 @@
+#include <stdio.h> // I/O
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef enum
+{
+    TOKEN_IDENTIFIER,
+    TOKEN_NUMBER,
+    TOKEN_STRING,
+    TOKEN_KEYWORD,     
+    TOKEN_OPERATOR,    
+    TOKEN_PUNCTUATION, 
+    TOKEN_EOF,         
+    TOKEN_ERROR  
+} TokenType;
+
+typedef struct
+{
+    TokenType type; //Find what token it is
+    char *value;    //Token value
+    int line;       //What line is it in the file
+    int column;
+} Token; 
+
+typedef struct
+{
+    const char *source; //Whole code
+    int current_pos;    //... pretty obvious
+    int line;           //where ma errors at
+    int column;
+} Lexer; //detects where errors are in code
+
+Lexer* init_lexer(const char *source) //had to search this one up
+{
+    Lexer *lexer = (lexer*)malloc(siseof(lexer));
+    if (!Lexer)
+    {
+        fprintf(stderr, "memory allocation failed\n");
+        exit(1);
+    }
+
+    lexer -> source = source;
+    lexer -> current_pos = 0;
+    lexer -> line = 1;
+    lexer -> column = 1;
+
+    return lexer;
+}
+
+void advance(Lexer *lexer)
+{
+    if (lexer -> source[lexer -> current_pos] == \n) //go to the struct lexer and grabs source. Then in the source, find the current position of the new line.
+    {
+        lexer -> line++; //go to next line
+        lexer -> column = 1; //reset column at new line
+    }
+    else
+    {
+        lexer -> column++; //continue to next column
+    }
+    lexer -> current_pos++; //always advances, would get stuck without it 
+}
+
+char peek(Lexer *lexer) //returns current character without advancing
+{
+    return lexer -> source[lexer -> current_pos]; //charAt(i)
+}
+
+char peek_next(Lexer *lexer)
+{
+    return lexer -> source[lexer -> current_pos + 1]; //charAt(i + 1)
+}
+
+int is_whitespace(char c) //check if current position is whitespace
+{
+    return c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\f' || c == '\v';
+}
+
+void skip_whitespace(Lexer *lexer)
+{
+    while (is_whitespace(peek(lexer)))
+    {
+        advance(lexer);
+    }
+}
