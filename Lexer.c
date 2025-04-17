5601cee733273a0afca8f66f64371b60a8243678
zappro74 <150842322+zappro74@users.noreply.github.com>
2025-04-16 21:12:26 -0500

    Commit 04/16/25
    Added all keywords,
    Created/completed read_identifier() method,
    Created/completed create_token() method

--- Lexer.c
+++ Lexer.c
@@ -1,7 +1,7 @@
 #include <stdio.h> // I/O
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+#include <ctype.h> //for isalpha()/isdigit() function
 
 typedef enum
 {
@@ -24,24 +24,56 @@ typedef struct
 //keyword table, used in the read_identifier() method to distinguish keyword from identifier.
 static const Keyword keywords[] //keywords cannot be used as an identifier. identifiers are variables that are named whatever besides these keywords.
 {
-    {"if", TOKEN_KEYWORD_IF},
-    {"else", TOKEN_KEYWORD_ELSE},
+    {"abstract", TOKEN_KEYWORD_ABSTRACT},
+    {"continue", TOKEN_KEYWORD_CONTINUE},
     {"for", TOKEN_KEYWORD_FOR},
-    {"while", TOKEN_KEYWORD_WHILE},
-    {"do", TOKEN_KEWORD_DO},
-    {"return", TOKEN_KEYWORD_RETURN},
     {"new", TOKEN_KEYWORD_NEW},
     {"switch", TOKEN_KEYWORD_SWITCH},
-    {"continue", TOKEN_KEYWORD_CONTINUE},
-    {"abstract", TOKEN_KEYWORD_ABSTRACT},
-    {"default", TOKEN_KEYWORD_DEFAULT},
     {"assert", TOKEN_KEYWORD_ASSERT},
+    {"default", TOKEN_KEYWORD_DEFAULT},
     {"goto", TOKEN_KEYWORD_GOTO},
     {"package", TOKEN_KEYWORD_PACKAGE},
     {"synchronized", TOKEN_KEYWORD_SYNCHRONIZED},
     {"boolean", TOKEN_KEYWORD_BOOLEAN},
+    {"do", TOKEN_KEYWORD_DO},
+    {"if", TOKEN_KEYWORD_IF},
     {"private", TOKEN_KEYWORD_PRIVATE},
     {"this", TOKEN_KEYWORD_THIS},
+    {"break", TOKEN_KEYWORD_BREAK},
+    {"double", TOKEN_KEYWORD_DOUBLE},
+    {"implements", TOKEN_KEYWORD_IMPLEMENTS},
+    {"protected", TOKEN_KEYWORD_PROTECTED},
+    {"throw", TOKEN_KEYWORD_THROW},
+    {"byte", TOKEN_KEYWORD_BYTE},
+    {"else", TOKEN_KEYWORD_ELSE},
+    {"import", TOKEN_KEYWORD_IMPORT},
+    {"public", TOKEN_KEYWORD_PUBLIC},
+    {"throws", TOKEN_KEYWORD_THROWS},
+    {"case", TOKEN_KEYWORD_CASE},
+    {"enum", TOKEN_KEYWORD_ENUM},
+    {"instanceof", TOKEN_KEYWORD_INSTANCEOF},
+    {"return", TOKEN_KEYWORD_RETURN},
+    {"transient", TOKEN_KEYWORD_TRANSIENT},
+    {"catch", TOKEN_KEYWORD_CATCH},
+    {"extends", TOKEN_KEYWORD_EXTENDS},
+    {"int", TOKEN_KEYWORD_INT},
+    {"short", TOKEN_KEYWORD_SHORT},
+    {"try", TOKEN_KEYWORD_TRY},
+    {"char", TOKEN_KEYWORD_CHAR},
+    {"final", TOKEN_KEYWORD_FINAL},
+    {"interface", TOKEN_KEYWORD_INTERFACE},
+    {"static", TOKEN_KEYWORD_STATIC},
+    {"void", TOKEN_KEYWORD_VOID},
+    {"class", TOKEN_KEYWORD_CLASS},
+    {"finally", TOKEN_KEYWORD_FINALLY},
+    {"long", TOKEN_KEYWORD_LONG},
+    {"strictfp", TOKEN_KEYWORD_STRICTFP},
+    {"volatile", TOKEN_KEYWORD_VOLATILE},
+    {"const", TOKEN_KEYWORD_CONST},
+    {"float", TOKEN_KEYWORD_FLOAT},
+    {"native", TOKEN_KEYWORD_NATIVE},
+    {"super", TOKEN_KEYWORD_SUPER},
+    {"while", TOKEN_KEYWORD_WHILE}
 }
 
 typedef struct
@@ -60,7 +92,35 @@ typedef struct
     int column;
 } Lexer; //detects where errors are in code
 
-Lexer* init_lexer(const char *source) //had to search this one up
+Token* create_token(TokenType type, char* lexeme, int line, int column) //creates a token with the inputs of the TokenType, the raw string, and the location in the source
+{
+    Token* token = (Token*)malloc(sizeof(Token));
+
+    if (!token)
+    {
+        fprintf(stderr, "Error: Failed to allocate memory for Token\n");
+        return NULL;
+    }
+
+    char* lexeme_copy = (char*)malloc(strlen(lexeme) + 1); //add 1 to the string length because strings are null terminated in C. which means there is an extra byte at the end of a string.
+    if (!lexeme_copy)
+    {
+        fprintf(stderr, "Error: Failed to allocate memory for lexeme\n");
+        free(token); //free the memory location of the token struct, since there is no longer a token struct being created
+        return NULL;
+    }
+
+    strcpy(lexeme_copy, lexeme); //copies content from lexeme to lexeme_copy
+
+    token -> type = type;
+    token -> value = lexeme_copy;
+    token -> line = line;
+    token -> column = column;
+
+    return token;
+}
+
+Lexer* init_lexer(const char *source) //had to search this one up... will get back to it later
 {
     Lexer *lexer = (lexer*)malloc(siseof(lexer));
     if (!Lexer)
@@ -114,7 +174,53 @@ void skip_whitespace(Lexer *lexer)
     }
 }
 
+int is_identifier_start(char c)
+{
+    return isalpha(c) || c == "_" || c == '$';
+}
+
+int is_identifier(char c)
+{
+    return isalpha(c) || isdigit(c) || c == "_" || c == '$';
+}
+
 Token* read_identifier(Lexer *lexer)
 {
+    if (!is_identifier_start(peek(lexer)))
+    {
+        return NULL;
+    }
+  
+    while (is_identifier(lexer))
+    {
+        advance(lexer);
+    }
+
+    int start_pos = lexer -> current_pos;
+
+    //start of lexme(raw text that forms a token) extraction
+    int length = lexer -> current_pos - start_pos;
+    char *lexeme = malloc(length + 1);
     
+    if (!lexeme) //check if memory allocation failed
+    {
+        fprintf(stderr, "Memory error: Could not allocate lexeme\n");
+        return NULL;
+    }
+
+    strncpy(lexeme, lexer -> source + start_pos, length); //copies identifier from the source into lexeme buffer
+    lexeme[length] = '\0'; //null termination again
+
+    TokenType type = TOKEN_IDENTIFIER; //assuming whats in the lexeme is an identifier
+
+    for (int i = 0; i < sizeof(keywords); i++)
+    {
+        if (strcmp(lexeme, keywords[i].keyword) == 0) //compares strings
+        {
+            type = keywords[i].type;
+            break;
+        }
+    }
+
+    return create_token(type, lexeme, lexer -> source[lexer -> line], lexer -> source[lexer -> column])
 }
